0e580b7a37cf501028dc405010fbd83c325ea152
xayr0n <zxyron0@gmail.com>
2022-08-23 02:02:02 +0530

    Fix scroll behaviour

--- lib/pages/library.dart
+++ lib/pages/library.dart
@@ -38,50 +38,57 @@ class _LibraryState extends State<Library> {
           child: ValueListenableBuilder(
             valueListenable: favouritePlaylistsListenable(),
             builder: (context, Box favouritePlaylistBox, _) {
-              return ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: favouritePlaylistBox.isNotEmpty
-                    ? favouritePlaylistBox.length
-                    : 0,
-                itemBuilder: (context, index) => TextButton(
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PlaylistPage(
-                        playlistId:
-                            favouritePlaylistBox.getAt(index)['playlistId']!,
-                        thumbnail:
-                            favouritePlaylistBox.getAt(index)['thumbnail']!,
+              return NotificationListener<OverscrollNotification>(
+                // Suppress OverscrollNotification events that escape from the inner scrollable
+                onNotification: (notification) =>
+                    notification.metrics.axisDirection != AxisDirection.down,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: favouritePlaylistBox.isNotEmpty
+                      ? favouritePlaylistBox.length
+                      : 0,
+                  itemBuilder: (context, index) => TextButton(
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PlaylistPage(
+                          playlistId:
+                              favouritePlaylistBox.getAt(index)['playlistId']!,
+                          thumbnail:
+                              favouritePlaylistBox.getAt(index)['thumbnail']!,
+                        ),
                       ),
                     ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: CachedNetworkImage(
-                          imageUrl:
-                              favouritePlaylistBox.getAt(index)['thumbnail']!,
-                          width: PlayerManager.size.width * 0.4,
-                          height: PlayerManager.size.width * 0.4,
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      SizedBox(
-                        width: PlayerManager.size.width * 0.4,
-                        child: Text(
-                          favouritePlaylistBox.getAt(index)['title'].toString(),
-                          textAlign: TextAlign.center,
-                          maxLines: 1,
-                          style: TextStyle(
-                            fontSize: PlayerManager.size.width * 0.04,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                favouritePlaylistBox.getAt(index)['thumbnail']!,
+                            width: PlayerManager.size.width * 0.4,
+                            height: PlayerManager.size.width * 0.4,
                           ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 10.0),
+                        SizedBox(
+                          width: PlayerManager.size.width * 0.4,
+                          child: Text(
+                            favouritePlaylistBox
+                                .getAt(index)['title']
+                                .toString(),
+                            textAlign: TextAlign.center,
+                            maxLines: 1,
+                            style: TextStyle(
+                              fontSize: PlayerManager.size.width * 0.04,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
